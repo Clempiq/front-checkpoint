e69c36d8bf1a1d75630ad214b5c9c94943e8b52c
Piquenet <clement.piquenet@gmail.com>
2023-05-22 16:10:41 +0200

    first

---
+++ src/pages/ContinentsPage.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
---
+++ src/pages/CountriesPage.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
